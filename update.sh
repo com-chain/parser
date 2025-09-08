435c89384aa725765d080fca84e131d6c362a3b8
Valentin Lab <valentin.lab@kalysto.org>
2025-09-09 01:57:13 +0200

    new: move files to ``parser/``
    The intent is to incrementally move to a cleaner setup. Here, we move
    to using git as reference code and update method.

--- update.sh
+++ update.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-cd /home/ethereum
+PARSER_DIR="$HOME/parser"
 while true
 do
   # if geth is not running, wait, checking every min
-  while [ ! -e .ethereum/geth.ipc ]
+  while [ ! -e "$HOME/.ethereum/geth.ipc" ]
   do
     sleep 60 # or less like 0.2
   done
 lastBlock=`geth --exec "eth.blockNumber" attach`
-firstBlock=`cat block.txt`
+firstBlock=`cat "$PARSER_DIR/block.txt"`
 if [ $lastBlock -ge $firstBlock ] 
 then
 	echo "Parsing $firstBlock - $lastBlock"
-	cat logtodb.TMPL | sed s/LBLOCK/$lastBlock/g | sed s/FBLOCK/$firstBlock/g > logtodb
-	geth --exec 'loadScript("logtodb")' attach | ./parser.py
+	cat "$PARSER_DIR/logtodb.TMPL" | sed s/LBLOCK/$lastBlock/g | sed s/FBLOCK/$firstBlock/g > "$PARSER_DIR/logtodb"
+	geth --exec 'loadScript("'"$PARSER_DIR"'/logtodb")' attach | "$PARSER_DIR/parser.py"
 	nextBlock=`expr $lastBlock + 1`
-	echo $nextBlock > block.txt
+	echo $nextBlock > "$PARSER_DIR"/block.txt
 fi
 #sleep 2
 done
--- watchdog.sh
+++ watchdog.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-cd /home/ethereum
+PARSER_DIR="$HOME/parser"
+
+cd "$PARSER_DIR"
 while true
 do
         update_pid=`pgrep update.sh`
 	if [ -z "$update_pid" ]
 	then
-		nohup /home/ethereum/update.sh >> /home/ethereum/parser.log 2>&1 &
+		nohup "$PARSER_DIR/update.sh" >> "$PARSER_DIR/parser.log" 2>&1 &
 		echo restarted update.sh
 	fi
 sleep 10
